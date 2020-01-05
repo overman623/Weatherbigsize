3137319019300c26544e8b33bad4b9b9b35868d0
overman623 <overman623@naver.com>
2020-01-05 20:13:41 +0900

    feat : write basic layout set

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(SampleApp());
+}
 
-class MyApp extends StatelessWidget {
+class SampleApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner : false,
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-          appBar: AppBar (
-            title: Text('Welcome to Flutter'),
+      home: SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  SampleAppPage({Key key}) : super(key: key);
+
+  @override
+  _SampleAppPageState createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  // Default placeholder text
+  String textToShow = "";
+
+  void _updateText() {
+    setState(() {
+      // update the text
+      textToShow = "Flutter is Awesome!";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://images.unsplash.com/photo-1517030330234-94c4fb948ebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1275&q=80'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(
+              child: Text(
+                'mrflutter.com',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+            ),
           ),
-          body: Center(
-            child: Text('Hello world')
+          Positioned(
+            child: AppBar(
+              title: Text("Weather Alarm"),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.share),
+                  onPressed: () {},
+                  tooltip: 'Share',
+                ),
+              ],
+            ),
           )
+        ],
       ),
     );
   }
+
 }
